79fa8780c2bbe816579a4101867b34134a59ad64
Gal Alfandary <gala29199@gmail.com>
2024-12-09 11:32:21 +0200

    added contact buttons

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".Linkedin").click(function () {
+    // Replace the URL with your LinkedIn profile link
+    window.open("https://www.linkedin.com/in/gal-alfandary-351b8718a/", "_blank");
+  });
+  $(".Email").click(function () {
+    // Replace the email address with your email
+    window.open("mailto:gala29199@gmail.com", "_blank");
+  } );
+  $(".Whatsapp").click(function () {
+    // Replace the phone number with your phone number
+    window.open("https://wa.me/972508168774", "_blank");
+  }); 
+});
